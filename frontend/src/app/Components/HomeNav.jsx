0ab2eebb9c4dbd4446272f76da206d3c5e2a9828
Harsh Telang <telangharsh06@gmail.com>
2024-02-08 21:54:45 +0530

    Search functionality fixed

--- frontend/src/app/Components/HomeNav.jsx
+++ frontend/src/app/Components/HomeNav.jsx
@@ -1,11 +1,13 @@
 'use client'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
-import { IoMdSearch } from "react-icons/io"
+import Search from "../Components/Search"
 
 
 
 const HomeNav = () => {
+  const router = useRouter();
   const [query, setQuery] = useState("");
   const [kebabQuery, setKebabQuery] = useState("");
 
@@ -17,10 +19,8 @@ const HomeNav = () => {
   function toKebabCase(str) {
     return str.replace(/\s+/g, '-').toLowerCase();
   }
-  
   const userInput = query;
   const kebabCaseString = toKebabCase(userInput);
-
   return (
     <div className='h-[70px] w-[100vw] justify-between flex bg-red-100 px-3 z-50 fixed homenav'>
         <div className='float-left bg-slate-50 w-[36%] flex justify-between'>
@@ -28,17 +28,7 @@ const HomeNav = () => {
 
           </div>
           <div className='bg-red-500 w-[65%] h-full py-4'>
-            <div className='h-full w-full bg-white flex items-center'>
-              <input type='text' className='w-[75%] border-none h-full' onChange={(e)=>{
-                setQuery(e.target.value);
-              }}  />
-              <button onClick={handleSearch} className='w-[10%]'>
-                <IoMdSearch className="text-black text-xl font-bold ml-1" />
-              </button>
-              <button className='w-[15%] h-[75%] mr-2 bg-gray-400 rounded-sm text-white flex justify-center items-center text-sm'>
-                Filter
-              </button>
-            </div>
+            <Search />
           </div>
 
         </div>
