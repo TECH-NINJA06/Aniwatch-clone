07bf742a0ea2c655b18d2a6945a3ed355ce9a646
Harsh Telang <telangharsh06@gmail.com>
2024-02-17 18:17:25 +0530

    bug fixes

--- frontend/src/app/anime/page.jsx
+++ frontend/src/app/anime/page.jsx
@@ -1,44 +1,90 @@
-'use client'
+"use client";
 
-import React, { useState, useEffect } from 'react'
-import { useSearchParams } from 'next/navigation'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import axios from "axios";
+import HomeNav from "../Components/HomeNav";
+import { ANIMES } from "../utils/data.example";
+import { BsFillBadgeCcFill } from "react-icons/bs";
+import { CiMicrophoneOn } from "react-icons/ci";
 
 const page = () => {
-    const [animes, setAnimes] = useState({})
+  const [animes, setAnimes] = useState({});
 
-    const searchParams = useSearchParams()
- 
-    const id = searchParams.get('id')
+  const searchParams = useSearchParams();
 
-    useEffect(() => {
-        ;( async() => {
-          try {
-            const response = await axios.get(`https://api-aniwatch.onrender.com/anime/info?id=${id}`);
-            const data = await response.data
-            setAnimes(data)
-            // await setSpotlight(home.spotlightAnimes)
-            // await setSpotlight1(spotlight[0])
-            // setPosterUrl1(spotlight1.poster)
-            //  console.log(home.spotlightAnimes[0].poster)
-            console.log(animes)
-            // console.log(spotlight1)
-            // console.log(posterUrl1)
-          } catch (error) {
-            console.log(error);
-          }
-        })()
-      
-      }, [])
+  const id = searchParams.get("id");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(
+          `https://api-aniwatch.onrender.com/anime/info?id=${id}`
+        );
+        const data = await response.data;
+        setAnimes(data);
+        // await setSpotlight(home.spotlightAnimes)
+        // await setSpotlight1(spotlight[0])
+        // setPosterUrl1(spotlight1.poster)
+        //  console.log(home.spotlightAnimes[0].poster)
+        console.log(animes);
+        // console.log(spotlight1)
+        // console.log(posterUrl1)
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
   return (
-    <div>
-      <h2>Hello</h2>
-      <h1>{animes?.anime?.info?.id}</h1>
+    <div className=" h-[100vh] w-[100vw] flex">
+      <HomeNav />
+      <div className="h-[75vh] w-full mt-28 flex justify-between">
+        <div className="h-full w-[29%]">
+          <div className=" anime h-[55%] w-[60%] flex justify-between items-cente mx-10 my-5">
+            {/* <img src={animes?.anime?.info?.poster} alt="Anime img" className='h-full w-[5rem]' /> */}
+            <img
+              src={ANIMES.anime.info.poster}
+              alt="Anime img"
+              className="h-full w-full aspect-video rounded-lg"
+            />
+          </div>
+        </div>
+        <div className="bg-blue-400 h-full w-[69%] mr-10 pl-2">
+          <div className="flex items-center text-[#ffdd95] font-bold text-4xl leading-[1.3] h-10 mb-12">
+            <h1>{ANIMES.anime.info.name}</h1>
+          </div>
+          <div className="h-10 w-full flex items-center gap-3">
+            <div className="h-7 w-10 flex justify-center items-center
+             bg-white text-black my-2 rounded">
+              <h1 className=" font-bold text-center">{ANIMES.anime.info.stats.rating}</h1>
+            </div>
+            <div className="h-7 w-10 flex justify-center items-center
+             bg-[#ffdd95] text-black my-2 rounded">
+               <h1 className=" font-bold text-center">{ANIMES.anime.info.stats.quality}</h1>
+            </div>
+            <div className="h-7 w-10 flex justify-center items-center
+             bg-green-400 text-black my-2 rounded gap-1">
+              <BsFillBadgeCcFill className='object-contain' />
+              <h1 className=" font-bold text-center">{ANIMES.anime.info.stats.episodes.sub}</h1>
+            </div>
+            <div className="h-7 w-10 flex justify-center items-center
+             bg-red-300 text-black my-2 rounded gap-1">
+               <CiMicrophoneOn className='object-contain' />
+              <h1 className=" font-bold text-center">{ANIMES.anime.info.stats.episodes.dub}</h1>
+            </div>
+          </div>
+          <div className="w-full h-20 bg-white mt-14">
+
+          </div>
+        </div>
+      </div>
+
+      {/* <h1>{animes?.anime?.info?.id}</h1>
       <p>{animes?.anime?.info?.name}</p>
       <img src={animes?.anime?.info?.poster} alt="" />
-      
-      </div>
-  )
-}
+       */}
+    </div>
+  );
+};
 
-export default page
+export default page;
